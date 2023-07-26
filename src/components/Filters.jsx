03033dd45841b0250142fc383238be652635fef7
ArkanHELLCL~ <lcastillop1975@hotmail.com.com>
2023-07-26 18:51:15 -0400

    Search funcional sin form ni submit

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -72,14 +72,11 @@ export function Filters () {
         }))
     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault()        
-    }
-
     const handleChange = (e) => {        
         setFilters(prevState => ({
             ...prevState, 
-            search: e.target.value            
+            search: e.target.value,
+            page: 1
         }))        
     }
     
@@ -91,20 +88,16 @@ export function Filters () {
                         <span className ="bg-green-200 text-green-800 text-xl font-medium mr-2 px-2.5 py-0.5 pb-1 rounded-full dark:bg-green-900 dark:text-green-300">Disponibles : {totalItemesAvailable}</span>
                     </div>
                 </div>
-                <div className="grid">
-                    <form onSubmit={handleSubmit}>   
-                        <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                        <div className="relative">
-                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                                </svg>
-                            </div>
-                            <input name="query" onChange={handleChange} value={search} placeholder='Harry Potter, Dracula ...' className={`block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 `} />
-
-                            <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                <div className="grid">                    
+                    <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                    <div className="relative">
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                            </svg>
                         </div>
-                    </form>
+                        <input name="query" onChange={handleChange} value={search} placeholder='Harry Potter, Dracula ...' className={`block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 `} />                            
+                    </div>                    
                     {error && <p className="text-red-500 text-xs italic pt-2">{error}</p>}
                 </div>                
             </section>
